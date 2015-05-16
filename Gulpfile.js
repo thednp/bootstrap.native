fa8f803954954f6ab11252a3a3f070272b49385a
Ingwie Phoenix <ingwie2000@googlemail.com>
2015-05-16 17:20:07 +0200

    Updated build process, reverting build locations and fixing name.

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,6 +3,10 @@ var uglify = require('gulp-uglify');
 var rename = require("gulp-rename");
 var concat = require('gulp-concat');
 
+var distName = "bootstrap-native";
+var distDir = "dist/";
+var minDir = "lib/min/";
+
 gulp.task('default', function() {
     // Minify the library files only...
     gulp.src('lib/*.js')
@@ -10,16 +14,16 @@ gulp.task('default', function() {
     .pipe(rename({
         suffix: ".min"
     }))
-    .pipe(gulp.dest('dist/lib'));
+    .pipe(gulp.dest(minDir));
 
     // Minify everything into a distribution.
     gulp.src('lib/*.js')
-    .pipe(concat("bootstrap.native.min.js"))
+    .pipe(concat(distName+".min.js"))
     .pipe(uglify())
-    .pipe(gulp.dest('dist/'));
+    .pipe(gulp.dest(distDir));
 
     // Just concat
     gulp.src('lib/*.js')
-    .pipe(concat("bootstrap.native.js"))
-    .pipe(gulp.dest('dist/'));
+    .pipe(concat(distName+".js"))
+    .pipe(gulp.dest(distDir));
 });
