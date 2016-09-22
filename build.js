294c04503caa5c6af4d4d2b299baf559508433ee
thednp <thednp@users.noreply.github.com>
2016-09-23 00:35:17 +0300

    Changed build script to add version to the files

--- build.js
+++ build.js
@@ -6,7 +6,34 @@
 var fs = require('fs');
 var path = require('path');
 var uglify = require('uglify-js');
-console.log('Minified:');
+var pack = require('./package.json');
+var version = 'v'+pack.version;
+var license = pack.license+'-License';
+
+console.log('Minifying ' + version + '..');
+
+// Helper Functions:
+function ucfirst(str) {
+    var cap = str.charAt(0).toUpperCase();
+    return cap+str.substr(1);
+}
+function replaceB(s){
+  if (/boo/.test(s)) { return ''; } 
+  else { return " | " + ucfirst(s.replace(/lib\\|\-native.js/g,'')); }
+}
+function minify(srcPath, writePath) {
+  fs.writeFile(writePath, 
+    ('// Native Javascript for Bootstrap 3 ' + version + ' | © dnp_theme' + replaceB(srcPath)  + ' | ' + license + '\n'
+    + uglify.minify(srcPath).code ), function (err) {
+    if (err) return handleError(err);
+    console.log(srcPath);
+  });
+}
+function handleError(err) {
+  console.error(err);
+  process.exit(1);
+}
+
 // Minify lib/
 fs.readdir('lib/', function (err, files) {
   if (err) return handleError(err);
@@ -23,14 +50,4 @@ fs.readdir('lib/', function (err, files) {
 });
 // Minify dist/bootstrap-native.js
 minify('dist/bootstrap-native.js', 'dist/bootstrap-native.min.js');
-// Helper Functions:
-function minify(srcPath, writePath) {
-  fs.writeFile(writePath, (uglify.minify(srcPath).code + '\n'), function (err) {
-    if (err) return handleError(err);
-    console.log(srcPath);
-  });
-}
-function handleError(err) {
-  console.error(err);
-  process.exit(1);
-}
+
