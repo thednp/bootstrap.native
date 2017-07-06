f3e28463fae11f35ef83d9b94415d41a789d0566
thednp <thednp@users.noreply.github.com>
2017-07-06 16:42:01 +0300

    Unnest Collapse as described here https://github.com/thednp/bootstrap.native/issues/139

--- dist/bootstrap-native.js
+++ dist/bootstrap-native.js
@@ -841,15 +841,13 @@
         setTimeout(function() {
           collapseElement[style][height] = getMaxHeight(collapseElement) + 'px';
   
-          (function(){
-            emulateTransitionEnd(collapseElement, function() {
-              isAnimating = false;
-              collapseElement[setAttribute](ariaExpanded,'true');
-              removeClass(collapseElement,collapsing);
-              collapseElement[style][height] = '';
-              bootstrapCustomEvent.call(collapseElement, shownEvent, component);
-            });
-          }());
+          emulateTransitionEnd(collapseElement, function() {
+            isAnimating = false;
+            collapseElement[setAttribute](ariaExpanded,'true');
+            removeClass(collapseElement,collapsing);
+            collapseElement[style][height] = '';
+            bootstrapCustomEvent.call(collapseElement, shownEvent, component);
+          });
         },20);
       },
       closeAction = function(collapseElement) {
@@ -860,16 +858,14 @@
           addClass(collapseElement,collapsing);
           collapseElement[style][height] = '0px';
   
-          (function(){
-            emulateTransitionEnd(collapseElement, function() {
-              isAnimating = false;
-              collapseElement[setAttribute](ariaExpanded,'false');
-              removeClass(collapseElement,collapsing);
-              removeClass(collapseElement,inClass);
-              collapseElement[style][height] = '';
-              bootstrapCustomEvent.call(collapseElement, hiddenEvent, component);
-            });
-          }());
+          emulateTransitionEnd(collapseElement, function() {
+            isAnimating = false;
+            collapseElement[setAttribute](ariaExpanded,'false');
+            removeClass(collapseElement,collapsing);
+            removeClass(collapseElement,inClass);
+            collapseElement[style][height] = '';
+            bootstrapCustomEvent.call(collapseElement, hiddenEvent, component);
+          });
         },20);
       },
       getTarget = function() {
