65a9396164b09f842403848c9f51fca8935efe24
thednp <thednp@users.noreply.github.com>
2017-09-27 21:28:08 +0300

    Changes: * Improved **ScrollSpy** with `offset` option * fixed an issue with **Modal** setting scrollbar on IE * code cleanup * documentation updates

--- dist/bootstrap-native.js
+++ dist/bootstrap-native.js
@@ -1,4 +1,4 @@
-// Native Javascript for Bootstrap 3 v2.0.15 | © dnp_theme | MIT-License
+// Native Javascript for Bootstrap 3 v2.0.16 | © dnp_theme | MIT-License
 (function (root, factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD support:
@@ -708,7 +708,7 @@
   
       bootstrapCustomEvent.call(element, slideEvent, component, slides[next]); // here we go with the slide
   
-      isSliding = this.isSliding = true;
+      isSliding = true;
       clearInterval(timer);
       setActivePage( next );
   
@@ -722,7 +722,7 @@
         one(slides[activeItem], transitionEndEvent, function(e) {
           var timeout = e[target] !== slides[activeItem] ? e.elapsedTime*1000 : 0;
           setTimeout(function(){
-            isSliding = self.isSliding = false;
+            isSliding = false;
   
             addClass(slides[next],active);
             removeClass(slides[activeItem],active);
@@ -1025,7 +1025,7 @@
           body[style][paddingRight] = (bodyPad + scrollbarWidth) + 'px';
           if (fixedItems[length]){
             for (var i = 0; i < fixedItems[length]; i++) {
-              itemPad = globalObject.getComputedStyle(fixedItems[i])[paddingRight];
+              itemPad = (fixedItems[i].currentStyle || globalObject.getComputedStyle(fixedItems[i]))[paddingRight];
               fixedItems[i][style][paddingRight] = ( parseInt(itemPad) + scrollbarWidth) + 'px';
             }
           }
@@ -1419,7 +1419,8 @@
     element = queryElement(element); 
   
     // DATA API
-    var targetData = queryElement(element[getAttribute](dataTarget));
+    var targetData = queryElement(element[getAttribute](dataTarget)),
+        offsetData = element[getAttribute]('data-offset');
   
     // set options
     options = options || {};
@@ -1427,7 +1428,8 @@
   
     // event targets, constants
     var spyTarget = options[target] && queryElement(options[target]) || targetData,
-        links = spyTarget && spyTarget[getElementsByTagName]('A'), 
+        links = spyTarget && spyTarget[getElementsByTagName]('A'),
+        offset = parseInt(offsetData || options['offset']) || 10,      
         items = [], targetItems = [], scrollOffset,
         scrollTarget = element[offsetHeight] < element[scrollHeight] ? element : globalObject, // determine which is the real scrollTarget
         isWindow = scrollTarget === globalObject;  
@@ -1451,8 +1453,8 @@
   
           isActive = hasClass(parent,active) || false,
   
-          topEdge = isWindow ? targetRect[top] + scrollOffset : targetItem[offsetTop] - (targetItems[index-1] ? 0 : 10),
-          bottomEdge = isWindow ? targetRect[bottom] + scrollOffset : targetItems[index+1] ? targetItems[index+1][offsetTop] : element[scrollHeight],
+          topEdge = (isWindow ? targetRect[top] + scrollOffset : targetItem[offsetTop]) - offset,
+          bottomEdge = isWindow ? targetRect[bottom] + scrollOffset - offset : targetItems[index+1] ? targetItems[index+1][offsetTop] - offset : element[scrollHeight],
   
           inside = scrollOffset >= topEdge && bottomEdge > scrollOffset;
   
