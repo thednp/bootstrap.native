2a26312d6c44dfcf5e5d24364d856907b211d1be
thednp <thednp@users.noreply.github.com>
2020-01-03 07:57:45 +0200

    Changes:
    * removed `lib` folder, now everything is in the legacy `branches/legacy/` branch
    * added banner to minified builds
    * as suggested by @cvaize, moved `supports.push(component)` to `init.js`
    * minor fixes to pollyfills
    * removed bower.json

--- src/util/globals.js
+++
@@ -1,5 +0,0 @@
-// globals
-// const globalObject = typeof global !== 'undefined' ? global : window;
-// const BSN = globalObject.BSN = {};
-// export const supports = globalObject.BSN.supports = [];
-export const supports = /*globalObject.BSN.supports =*/ [];
--- src/util/init.js
+++
@@ -1,5 +0,0 @@
-import { initCallback } from './callbacks.js'
-import { on } from './event.js'
-
-// bulk initialize all components
-document.body ? initCallback() : on( document, 'DOMContentLoaded', initCallback );
