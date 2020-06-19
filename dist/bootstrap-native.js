cded1437fb57cb36bb486eb47b803883b732f44d
thednp <thednp@users.noreply.github.com>
2020-06-19 05:20:08 +0000

    Bump version

--- dist/bootstrap-native.js
+++ dist/bootstrap-native.js
@@ -1,5 +1,5 @@
 /*!
-  * Native JavaScript for Bootstrap v3.0.6 (https://thednp.github.io/bootstrap.native/)
+  * Native JavaScript for Bootstrap v3.0.7 (https://thednp.github.io/bootstrap.native/)
   * Copyright 2015-2020 © dnp_theme
   * Licensed under MIT (https://github.com/thednp/bootstrap.native/blob/master/LICENSE)
   */
@@ -1637,7 +1637,7 @@
     }
   }
 
-  var version = "3.0.6";
+  var version = "3.0.7";
 
   var index = {
     Alert: Alert,
